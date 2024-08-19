aad57d77a2c6e9db8400f3f049ad3d6c279c74ad
Daniel Yoo <elitenick57@gmail.com>
2024-08-19 16:17:17 -0700

    Run migrate, add Item model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+  label_text = models.CharField(max_length=200)
+  added_date = models.DateTimeField("date added")
+  is_done = models.BooleanField()
+
+  def __str__(self):
+    return self.label_text
